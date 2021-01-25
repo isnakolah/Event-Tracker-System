d2aefb9ce9e17e9ba20047db778d7b9a7852a754
Daniel Nakolah <nakolahdaniel1@gmail.com>
2021-01-25 16:07:10 +0300

    Set default values

--- server/Models/ServiceResponse.cs
+++ server/Models/ServiceResponse.cs
@@ -3,8 +3,7 @@ namespace poneaChallenge.Models
     public class ServiceResponse<T>
     {
         public T Data { get; set; }
-        public bool Success { get; set; }
-        public string Error { get; set; }
+        public bool Success { get; set; } = true;
+        public string Error { get; set; } = null;
     }
 }
-C
