3afbb5ac62a383d799b070e81c206eb3bbfd662f
Daniel Nakolah <nakolahdaniel1@gmail.com>
2021-01-25 15:06:43 +0300

    SetUp the task Model

---
+++ server/Models/Task.cs
@@ -0,0 +1,10 @@
+namespace poneaChallenge.Models
+{
+    public class Task
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public int Precidence { get; set; }
+        public int Seconds { get; set; }
+    }
+}
