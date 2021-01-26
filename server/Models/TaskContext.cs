3f16833b69217cda6d301d3fc9a54ada3912bccb
Daniel Nakolah <nakolahdaniel1@gmail.com>
2021-01-26 07:59:21 +0300

    Create the TaskContext

---
+++ server/Models/TaskContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace poneaChallenge.Models
+{
+    public class TaskContext : DbContext
+    {
+        public TaskContext(DbContextOptions<TaskContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Task> Tasks { get; set; }
+    }
+}
