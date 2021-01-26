d6bf4cab1d3f91c33c18dcc9d80655d67cfcb6bd
Daniel Nakolah <nakolahdaniel1@gmail.com>
2021-01-26 08:00:32 +0300

    Register the service

--- server/Startup.cs
+++ server/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using poneaChallenge.Models;
 using poneaChallenge.TaskService;
 
 namespace server
@@ -27,6 +29,8 @@ namespace server
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<ITaskService, TaskService>();
+            services.AddDbContext<TaskContext>(
+                options => options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
         }
 
