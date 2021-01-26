47b9ecd8546a25f9e3f3d8a1e7c63790db9592bb
Daniel Nakolah <nakolahdaniel1@gmail.com>
2021-01-26 08:26:43 +0300

    Create a migration to update the Task Model

---
+++ server/Migrations/20210126052546_UpdateTaskModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace server.Migrations
+{
+    public partial class UpdateTaskModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Running",
+                table: "Tasks",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Running",
+                table: "Tasks");
+        }
+    }
+}
